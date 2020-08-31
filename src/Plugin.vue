d8be0116ad1fa10c666557494571f78380318595
Anders Højmose <anders@hojmose.com>
2020-08-31 15:44:31 +0200

    Initial commit

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -16,12 +16,31 @@
       v-bind:key="breakpoint.size"
     >
       <div class="box-model__margin relative">
-        <span class="box-model__title">margin</span>
-        <div class="box-model__margin-top">
+        
+        <div class="tree__form-group">
+          
+          <span class="form__topic">Content Horizontal Alignment</span>
+          
           <select
-            class="select-breakpoint"
+            class="uk-width-1-1"
             v-if="model[breakpoint.size]"
-            v-model="model[breakpoint.size].mt"
+            v-model="model[breakpoint.size].x"
+          >
+            <option :key="option" v-for="option in optionsHorizontal" :value="option">
+              {{
+              option
+              }}
+            </option>
+          </select>
+        </div>
+        
+        <div class="tree__form-group">
+          
+          <span class="form__topic">Content Vertical Alignment</span>
+          <select
+            class="uk-width-1-1"
+            v-if="model[breakpoint.size]"
+            v-model="model[breakpoint.size].y"
           >
             <option :key="option" v-for="option in options" :value="option">
               {{
@@ -30,101 +49,7 @@
             </option>
           </select>
         </div>
-        <div class="box-model__margin-bottom">
-          <select
-            class="select-breakpoint"
-            v-if="model[breakpoint.size]"
-            v-model="model[breakpoint.size].mb"
-          >
-            <option :key="option" v-for="option in options" :value="option">
-              {{
-              option
-              }}
-            </option>
-          </select>
-        </div>
-        <div class="box-model__margin-left">
-          <select
-            class="select-breakpoint"
-            v-if="model[breakpoint.size]"
-            v-model="model[breakpoint.size].ml"
-          >
-            <option :key="option" v-for="option in options" :value="option">
-              {{
-              option
-              }}
-            </option>
-          </select>
-        </div>
-        <div class="box-model__margin-right">
-          <select
-            class="select-breakpoint"
-            v-if="model[breakpoint.size]"
-            v-model="model[breakpoint.size].mr"
-          >
-            <option :key="option" v-for="option in options" :value="option">
-              {{
-              option
-              }}
-            </option>
-          </select>
-        </div>
-        <div class="box-model__padding relative">
-          <span class="box-model__title">padding</span>
-          <div class="box-model__padding-top">
-            <select
-              class="select-breakpoint"
-              v-if="model[breakpoint.size]"
-              v-model="model[breakpoint.size].pt"
-            >
-              <option :key="option" v-for="option in options" :value="option">
-                {{
-                option
-                }}
-              </option>
-            </select>
-          </div>
-          <div class="box-model__padding-bottom">
-            <select
-              class="select-breakpoint"
-              v-if="model[breakpoint.size]"
-              v-model="model[breakpoint.size].pb"
-            >
-              <option :key="option" v-for="option in options" :value="option">
-                {{
-                option
-                }}
-              </option>
-            </select>
-          </div>
-          <div class="box-model__padding-left">
-            <select
-              class="select-breakpoint"
-              v-if="model[breakpoint.size]"
-              v-model="model[breakpoint.size].pl"
-            >
-              <option :key="option" v-for="option in options" :value="option">
-                {{
-                option
-                }}
-              </option>
-            </select>
-          </div>
-          <div class="box-model__padding-right">
-            <select
-              class="select-breakpoint"
-              v-if="model[breakpoint.size]"
-              v-model="model[breakpoint.size].pr"
-            >
-              <option :key="option" v-for="option in options" :value="option">
-                {{
-                option
-                }}
-              </option>
-            </select>
-          </div>
-          <div class="box-model__dimensions relative">content</div>
-        </div>
+
       </div>
     </div>
   </div>
@@ -138,7 +63,8 @@ export default {
   data: function() {
     return {
       breakpoints,
-      options: ["-", "0", "xs", "s", "m", "l", "xl", "xxl"],
+      options: ["-", "start", "center", "end"],
+      optionsHorizontal: ["-", "start", "center", "end", "stretch"],
       currBreakpoint: "s"
     };
   },
@@ -146,19 +72,13 @@ export default {
     initWith() {
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: "spacing-plugin",
+        plugin: "alignment-plugin",
         ...breakpoints.reduce((acc, curr) => {
           return {
             ...acc,
             [curr.size]: {
-              pt: "-",
-              pb: "-",
-              pl: "-",
-              pr: "-",
-              mt: "-",
-              mb: "-",
-              ml: "-",
-              mr: "-"
+              x: "-",
+              y: "-",
             }
           };
         }, {})
@@ -186,14 +106,6 @@ export default {
 </script>
 
 <style>
-select {
-  text-align: center;
-  text-align-last: center;
-}
-option {
-  text-align: left;
-}
-
 .active {
   background-color: #efefef;
 }
@@ -204,161 +116,8 @@ option {
 }
 
 .button-group {
-  margin: 8px 0;
+  margin: 8px 0 32px 0;
   display: flex;
   align-self: flex-end;
-}
-
-.input-number {
-  text-align: center;
-  width: 24px !important;
-  height: 20px !important;
-  font-size: 10px !important;
-  padding: 0 !important;
-}
-
-.select-breakpoint {
-  text-align: center !important;
-  width: 24px !important;
-  height: 20px !important;
-  font-size: 10px !important;
-  padding: 0 !important;
-  background-image: none !important;
-}
-
-.box-model {
-  background-color: #efefef;
-  text-align: center;
-  font-size: 12px;
-  margin-bottom: 8px;
-}
-
-.box-model .relative {
-  position: relative;
-  padding: 24px 32px;
-}
-
-.box-model__margin {
-  background-color: #efefef;
-}
-
-.box-model__border {
-  background-color: #dfdfdf;
-}
-
-.box-model__padding {
-  background-color: #cfcfcf;
-}
-
-.box-model .box-model__dimensions {
-  background-color: #efefef;
-  padding: 8px 0px;
-}
-
-.box-model__title {
-  position: absolute;
-  line-height: 8px;
-  font-size: 8px;
-  top: 2px;
-  left: 2px;
-}
-
-/* margin */
-.box-model__margin-top {
-  position: absolute;
-  z-index: 1;
-  top: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__margin-bottom {
-  position: absolute;
-  z-index: 1;
-  bottom: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__margin-left {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  left: 2px;
-  transform: translateY(-50%);
-}
-
-.box-model__margin-right {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  right: 2px;
-  transform: translateY(-50%);
-}
-
-/* border */
-.box-model__border-top {
-  position: absolute;
-  z-index: 1;
-  top: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__border-bottom {
-  position: absolute;
-  z-index: 1;
-  bottom: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__border-left {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  left: 2px;
-  transform: translateY(-50%);
-}
-
-.box-model__border-right {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  right: 2px;
-  transform: translateY(-50%);
-}
-
-/* padding */
-.box-model__padding-top {
-  position: absolute;
-  z-index: 1;
-  top: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__padding-bottom {
-  position: absolute;
-  z-index: 1;
-  bottom: 2px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.box-model__padding-left {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  left: 2px;
-  transform: translateY(-50%);
-}
-
-.box-model__padding-right {
-  position: absolute;
-  z-index: 1;
-  top: 50%;
-  right: 2px;
-  transform: translateY(-50%);
 }
 </style>
--- src/datasource.js
+++ src/datasource.js
@@ -4,13 +4,3 @@ export const breakpoints = [
     { size: "l", name: "Large" },
     { size: "xl", name: "XL" }
   ];
-
-export const sizes = {
-  xxs: 2,
-  xs: 4,
-  s: 8,
-  m: 12,
-  l: 16,
-  xl: 24,
-  xxl: 32,
-};
